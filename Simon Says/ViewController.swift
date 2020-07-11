2ab707d8e808fe6b112a34ab65e569ec1c7b25f6
Adam Gusky <gusky.adam@uwlax.edu>
2020-07-10 21:50:37 -0500

    init

---
+++ Simon Says/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simon Says
+//
+//  Created by Adam Gusky on 7/10/20.
+//  Copyright © 2020 Adam Gusky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
